9de9f570270a7a75428d280308ffd022a43c6e0f
vistaero <vistaero22@hotmail.com>
2014-05-12 21:52:54 +0200

    Improved managing players.
    But it still needs a lot of work.

---
+++ CBLauncher/Players.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.PlayerNameColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.PlayerWhiteListedColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.IsOperatorColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ItIsBannedColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BanDateColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BanReasonColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BanExpiresColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BanSourceColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.PlayerUUIDColumn = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ToolStrip4 = New System.Windows.Forms.ToolStrip()
+        Me.WhiteListButton = New System.Windows.Forms.ToolStripButton()
+        Me.OperatorButton = New System.Windows.Forms.ToolStripButton()
+        Me.BannedButton = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.AddButton = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.AllowColumnReorder = True
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.PlayerNameColumn, Me.PlayerWhiteListedColumn, Me.IsOperatorColumn, Me.ItIsBannedColumn, Me.BanDateColumn, Me.BanReasonColumn, Me.BanExpiresColumn, Me.BanSourceColumn, Me.PlayerUUIDColumn})
+        resources.ApplyResources(Me.ListView1, "ListView1")
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable
+        Me.ListView1.MultiSelect = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'PlayerNameColumn
+        '
+        resources.ApplyResources(Me.PlayerNameColumn, "PlayerNameColumn")
+        '
+        'PlayerWhiteListedColumn
+        '
+        resources.ApplyResources(Me.PlayerWhiteListedColumn, "PlayerWhiteListedColumn")
+        '
+        'IsOperatorColumn
+        '
+        resources.ApplyResources(Me.IsOperatorColumn, "IsOperatorColumn")
+        '
+        'ItIsBannedColumn
+        '
+        resources.ApplyResources(Me.ItIsBannedColumn, "ItIsBannedColumn")
+        '
+        'BanDateColumn
+        '
+        resources.ApplyResources(Me.BanDateColumn, "BanDateColumn")
+        '
+        'BanReasonColumn
+        '
+        resources.ApplyResources(Me.BanReasonColumn, "BanReasonColumn")
+        '
+        'BanExpiresColumn
+        '
+        resources.ApplyResources(Me.BanExpiresColumn, "BanExpiresColumn")
+        '
+        'BanSourceColumn
+        '
+        resources.ApplyResources(Me.BanSourceColumn, "BanSourceColumn")
+        '
+        'PlayerUUIDColumn
+        '
+        resources.ApplyResources(Me.PlayerUUIDColumn, "PlayerUUIDColumn")
+        '
+        'ToolStrip4
+        '
+        Me.ToolStrip4.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip4.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.WhiteListButton, Me.OperatorButton, Me.BannedButton, Me.ToolStripSeparator1, Me.AddButton})
+        resources.ApplyResources(Me.ToolStrip4, "ToolStrip4")
+        Me.ToolStrip4.Name = "ToolStrip4"
+        '
+        'WhiteListButton
+        '
+        Me.WhiteListButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        resources.ApplyResources(Me.WhiteListButton, "WhiteListButton")
+        Me.WhiteListButton.Name = "WhiteListButton"
+        '
+        'OperatorButton
+        '
+        Me.OperatorButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        resources.ApplyResources(Me.OperatorButton, "OperatorButton")
+        Me.OperatorButton.Name = "OperatorButton"
+        '
+        'BannedButton
+        '
+        Me.BannedButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        resources.ApplyResources(Me.BannedButton, "BannedButton")
+        Me.BannedButton.Name = "BannedButton"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        resources.ApplyResources(Me.ToolStripSeparator1, "ToolStripSeparator1")
+        '
+        'AddButton
+        '
+        Me.AddButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        resources.ApplyResources(Me.AddButton, "AddButton")
+        Me.AddButton.Name = "AddButton"
+        '
+        'Form2
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.ToolStrip4)
+        Me.MinimizeBox = False
+        Me.Name = "Form2"
+        Me.ToolStrip4.ResumeLayout(False)
+        Me.ToolStrip4.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents PlayerNameColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents PlayerWhiteListedColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents IsOperatorColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ItIsBannedColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents BanDateColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents BanReasonColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents BanExpiresColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents BanSourceColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents PlayerUUIDColumn As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ToolStrip4 As System.Windows.Forms.ToolStrip
+    Friend WithEvents WhiteListButton As System.Windows.Forms.ToolStripButton
+    Friend WithEvents OperatorButton As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BannedButton As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents AddButton As System.Windows.Forms.ToolStripButton
+End Class
---
+++ CBLauncher/Players.vb
@@ -0,0 +1,319 @@
+﻿Imports Newtonsoft.Json
+
+
+Public Class Form2
+
+#Region "DeserializeJson"
+
+    Private WhiteListFile As String = Form1.JarFolder & "whitelist.json"
+    Private OperatorsFile As String = Form1.JarFolder & "ops.json"
+    Private BannedPlayersFile As String = Form1.JarFolder & "banned-players.json"
+    Private UserCacheFile As String = Form1.JarFolder & "usercache.json"
+
+    ' White List
+
+    Dim WhiteListJson As String = System.IO.File.ReadAllText(WhiteListFile)
+    Dim WhiteListPlayers As List(Of Player) = JsonConvert.DeserializeObject(Of List(Of Player))(WhiteListJson)
+
+    ' OPs
+
+    Dim OperatorsJson As String = System.IO.File.ReadAllText(OperatorsFile)
+    Dim OperatorsPlayers As List(Of Player) = JsonConvert.DeserializeObject(Of List(Of Player))(OperatorsJson)
+
+    ' Banned Players
+
+    Dim BannedPlayersJson As String = System.IO.File.ReadAllText(BannedPlayersFile)
+    Dim BannedPlayers As List(Of BannedPlayer) = JsonConvert.DeserializeObject(Of List(Of BannedPlayer))(BannedPlayersJson)
+
+    ' User Cache
+
+    Dim UserCacheJson As String = System.IO.File.ReadAllText(UserCacheFile)
+    Dim UserCacheList As List(Of UserCache) = JsonConvert.DeserializeObject(Of List(Of UserCache))(UserCacheJson)
+
+#End Region
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim WhiteListSerialized As String = JsonConvert.SerializeObject(WhiteListPlayers)
+        Dim OperatorsSerialized As String = JsonConvert.SerializeObject(OperatorsPlayers)
+        Dim BannedPlayersSerialized As String = JsonConvert.SerializeObject(BannedPlayers)
+
+        Try
+            IO.File.WriteAllText(WhiteListFile, WhiteListSerialized)
+            IO.File.WriteAllText(OperatorsFile, OperatorsSerialized)
+            IO.File.WriteAllText(BannedPlayersFile, BannedPlayersSerialized)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Me.Dispose()
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FillWithData()
+
+    End Sub
+
+    Private Sub FillWithData()
+        Dim PlayersTable As New List(Of ListViewItem)
+
+        Dim YesString As String = "Yes"
+        Dim NoString As String = "No"
+
+        For Each UserCache In UserCacheList
+            Dim PlayerToAdd As New ListViewItem(UserCache.name)
+            PlayerToAdd.Name = UserCache.name
+            For contador = 0 To 8
+                PlayerToAdd.SubItems.Add("")
+            Next
+            '' Start settings player attributes
+
+            ' Is in White List
+            PlayerToAdd.SubItems.Item(1).Text = NoString
+            For Each Player In WhiteListPlayers
+                If Player.uuid.Equals(UserCache.uuid) Then
+                    PlayerToAdd.SubItems.Item(1).Text = YesString
+                    Exit For
+                Else
+
+                End If
+            Next
+
+            ' Is an operator
+
+            For Each Player In OperatorsPlayers
+                If Player.uuid.Equals(UserCache.uuid) Then
+                    PlayerToAdd.SubItems.Item(2).Text = YesString
+                    Exit For
+                Else
+                    PlayerToAdd.SubItems.Item(2).Text = NoString
+                End If
+            Next
+
+            ' Is banned
+            For Each Player In BannedPlayers
+                If Player.uuid.Equals(UserCache.uuid) Then
+                    PlayerToAdd.SubItems.Item(3).Text = YesString
+                    PlayerToAdd.SubItems.Item(4).Text = Player.created
+                    PlayerToAdd.SubItems.Item(5).Text = Player.reason
+                    PlayerToAdd.SubItems.Item(6).Text = Player.expires
+                    PlayerToAdd.SubItems.Item(7).Text = Player.source
+                    Exit For
+                Else
+                    PlayerToAdd.SubItems.Item(3).Text = NoString
+                End If
+            Next
+
+            ' Player UUID
+            PlayerToAdd.SubItems.Item(8).Text = UserCache.uuid
+            ListView1.Items.Add(PlayerToAdd)
+        Next
+
+        ListView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+    End Sub
+
+    Private Sub WhiteListButton_Click(sender As Object, e As EventArgs) Handles WhiteListButton.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            Dim IsWhiteListed As Boolean = False
+
+            For Each Player In WhiteListPlayers
+                If Player.name.Equals(ListView1.FocusedItem.Text) Then
+                    IsWhiteListed = True
+                    WhiteListPlayers.Remove(Player)
+                    ListView1.Items.Clear()
+                    FillWithData()
+
+                    Exit For
+                End If
+
+            Next
+
+            If IsWhiteListed = False Then
+                For Each Usercache In UserCacheList
+                    If Usercache.name.Equals(ListView1.FocusedItem.Text) Then
+                        Dim NewWhiteListedPlayer As New Player
+                        NewWhiteListedPlayer.name = Usercache.name
+                        NewWhiteListedPlayer.uuid = Usercache.uuid
+                        WhiteListPlayers.Add(NewWhiteListedPlayer)
+                        ListView1.Items.Clear()
+                        FillWithData()
+                        Exit For
+                    End If
+                Next
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub OperatorButton_Click(sender As Object, e As EventArgs) Handles OperatorButton.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            Dim IsOperator As Boolean = False
+
+            For Each Player In OperatorsPlayers
+                If Player.name.Equals(ListView1.FocusedItem.Text) Then
+                    IsOperator = True
+                    OperatorsPlayers.Remove(Player)
+                    ListView1.Items.Clear()
+                    FillWithData()
+
+                    Exit For
+                End If
+
+            Next
+
+            If IsOperator = False Then
+                For Each Usercache In UserCacheList
+                    If Usercache.name.Equals(ListView1.FocusedItem.Text) Then
+                        Dim NewOperatorPlayer As New Player
+                        NewOperatorPlayer.name = Usercache.name
+                        NewOperatorPlayer.uuid = Usercache.uuid
+                        OperatorsPlayers.Add(NewOperatorPlayer)
+                        ListView1.Items.Clear()
+                        FillWithData()
+                        Exit For
+                    End If
+                Next
+            End If
+
+
+        End If
+    End Sub
+
+    Private Sub BannedButton_Click(sender As Object, e As EventArgs) Handles BannedButton.Click
+        If ListView1.SelectedItems.Count > 0 Then
+
+            For Each bannedPlayer In BannedPlayers
+                If bannedPlayer.name.Equals(ListView1.FocusedItem.Text) Then
+                    BannedPlayers.Remove(bannedPlayer)
+                    ListView1.Items.Clear()
+                    FillWithData()
+
+                    Exit For
+                End If
+
+            Next
+
+        End If
+    End Sub
+End Class
+
+Public Class Player
+    Public Property name() As String
+        Get
+            Return m_name
+        End Get
+        Set(value As String)
+            m_name = value
+        End Set
+    End Property
+    Private m_name As String
+
+    Public Property uuid() As String
+        Get
+            Return m_uuid
+        End Get
+        Set(value As String)
+            m_uuid = value
+        End Set
+    End Property
+    Private m_uuid As String
+End Class
+
+Public Class BannedPlayer
+
+    Public Property uuid() As String
+        Get
+            Return m_uuid
+        End Get
+        Set(value As String)
+            m_uuid = value
+        End Set
+    End Property
+    Private m_uuid As String
+
+    Public Property name() As String
+        Get
+            Return m_name
+        End Get
+        Set(value As String)
+            m_name = value
+        End Set
+    End Property
+    Private m_name As String
+
+    Public Property created() As String
+        Get
+            Return m_created
+        End Get
+        Set(value As String)
+            m_created = value
+        End Set
+    End Property
+    Private m_created As String
+
+    Public Property source() As String
+        Get
+            Return m_source
+        End Get
+        Set(value As String)
+            m_source = value
+        End Set
+    End Property
+    Private m_source As String
+
+    Public Property expires() As String
+        Get
+            Return m_expires
+        End Get
+        Set(value As String)
+            m_expires = value
+        End Set
+    End Property
+    Private m_expires As String
+
+    Public Property reason() As String
+        Get
+            Return m_reason
+        End Get
+        Set(value As String)
+            m_reason = value
+        End Set
+    End Property
+    Private m_reason As String
+
+End Class
+
+Public Class UserCache
+    Public Property name() As String
+        Get
+            Return m_name
+        End Get
+        Set(value As String)
+            m_name = value
+        End Set
+    End Property
+    Private m_name As String
+
+    Public Property uuid() As String
+        Get
+            Return m_uuid
+        End Get
+        Set(value As String)
+            m_uuid = value
+        End Set
+    End Property
+    Private m_uuid As String
+
+    Public Property expiresOn() As String
+        Get
+            Return m_expiresOn
+        End Get
+        Set(value As String)
+            m_expiresOn = value
+        End Set
+    End Property
+    Private m_expiresOn As String
+
+
+End Class
